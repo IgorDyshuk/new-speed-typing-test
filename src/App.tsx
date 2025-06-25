8cf3f91ab1ebedcf0b2e20ee49265225351dd93d
IgorDyshuk <igordysuk@gmail.com>
2025-06-25 14:01:03 +0300

    add API to fetch translated words from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import GamePage from "@/pages/GamePage.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 function App() {
+    const queryClient = new QueryClient()
+
     return (
         <div className="flex min-h-svh flex-col items-center justify-center">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<GamePage />} />
-                </Routes>
-            </BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<GamePage/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
         </div>
     )
 }
